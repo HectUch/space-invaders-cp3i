92c63a391c7b68432cd7f625acd05e60b833a016
HectUch <66647830+HectUch@users.noreply.github.com>
2021-05-25 14:43:35 +0200

    Update main.cpp
    Update for infinite loop

--- main.cpp
+++ main.cpp
@@ -14,10 +14,20 @@ int main()
     cout << gamer.getLives();
     sf::RenderWindow window(sf::VideoMode(800, 600), "Enschede Invaders");
     
+        while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    
     ScreenGen myGame(window);
     myGame.initPlayer(gamer);
      myGame.drawGame(window,0,0);
     
+    }
     return 0;
 }
 
